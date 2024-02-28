336ea175867764a802d90e427217e370f56a97a6
chloeekker <157659518+chloeekker@users.noreply.github.com>
2024-02-28 15:36:06 -0700

    initial simple builder

---
+++ gui_builder.py
@@ -0,0 +1,19 @@
+class DisplayBuilder:
+    def log_in(self):
+        pass
+
+    def search(self):
+        pass
+
+    def what_if(self):
+        pass
+
+class StudentDisplayBuilder(DisplayBuilder):
+    pass
+
+class FacultyDisplayBuilder(DisplayBuilder):
+    def add(self):
+        pass
+
+    def edit(self):
+        pass
