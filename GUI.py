2d8c23897ea79b2531d98aef833f9e8c46f25c7b
chloeekker <157659518+chloeekker@users.noreply.github.com>
2024-03-22 15:23:49 -0600

    add ColorScheme class

--- GUI.py
+++ GUI.py
@@ -1,7 +1,35 @@
 import streamlit as st
 import pandas as pd
 
+class ColorScheme:
+    def __init__(self):
+        self.bg_color = "#dee2e6"
+        self.bg_offset = "#ced4da"
+        self.primary = "#275d38"
+        self.secondary = "#416f4f"
+        self.white = "#ffffff"
+        self.black = "#000000"
+        self.title = "#275d38"
+        self.font1 = "#000000"
+        self.font2 = "#ffffff"
+    
+    def set_color_scheme_custom(self, primary : str, secondary : str):
+        self.primary = primary
+        self.secondary = secondary
 
+    def set_color_scheme_dark(self):
+        self.bg_color = "#275d38"
+        self.bg_offset = "#416f4f"
+        self.primary = self.white
+        self.secondary = "#ced4da"
+        self.font1 = self.white
+        self.font2 = self.black
+
+    def reset_to_default(self):
+        self.bg_color = "#dee2e6"
+        self.bg_offset = "#ced4da"
+        self.primary = "#275d38"
+        self.secondary = "#416f4f"
 
 def chat_bubble(sender, message, color):
     '''
@@ -12,7 +40,7 @@ def chat_bubble(sender, message, color):
                 f'<p style="margin: 0;">{sender}</p>'
                 f'<p style="margin: 0;">{message}</p>'
                 f'</div>', unsafe_allow_html=True)
-    
+
 
 
 def send(user, message, color):
