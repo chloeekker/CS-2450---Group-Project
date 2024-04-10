4671c0e6f3f132cd49b56726c971ffc26d11aa3c
chloeekker <157659518+chloeekker@users.noreply.github.com>
2024-04-10 15:02:00 -0600

    simplified ColorScheme

--- colors_class.py
+++ colors_class.py
@@ -1,29 +1,16 @@
 class ColorScheme:
     def __init__(self):
-        self.bg_color = "#dee2e6"
-        self.bg_offset = "#ced4da"
-        self.primary = "#275d38"
-        self.secondary = "#416f4f"
-        self.white = "#ffffff"
-        self.black = "#000000"
-        self.title = "#275d38"
-        self.font1 = "#000000"
-        self.font2 = "#ffffff"
+        self.font : str = None
+        self.border : str = None
+
+    def default(self):
+        self.font = "#275d38"
+        self.border = "#a7a8aa"
     
-    def set_color_scheme_custom(self, primary : str, secondary : str):
-        self.primary = primary
-        self.secondary = secondary
+    def gray_scale(self):
+        self.font = "#000000"
+        self.border = "#4d4d4d"
 
-    def set_color_scheme_dark(self):
-        self.bg_color = "#275d38"
-        self.bg_offset = "#416f4f"
-        self.primary = self.white
-        self.secondary = "#ced4da"
-        self.font1 = self.white
-        self.font2 = self.black
-
-    def reset_to_default(self):
-        self.bg_color = "#dee2e6"
-        self.bg_offset = "#ced4da"
-        self.primary = "#275d38"
-        self.secondary = "#416f4f"
+    def custom(self, primary : str, secondary : str):
+        self.font = primary
+        self.border = secondary
